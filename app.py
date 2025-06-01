11b90f39053811b8348f445c88ccc306d5499945
limonatasuyu <emredilek6@gmail.com>
2025-06-01 17:50:33 +0300

    Improvements made

--- app.py
+++ app.py
@@ -1,89 +1,103 @@
 from flask import Flask, request, render_template, redirect
 from models import db, EmployeeModel
+
 app = Flask(__name__)
 
 # Linking SQLite DB with SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 
-'''
+"""
 quote from documentation:
 
 If set to True, Flask-SQLAlchemy will track modifications of objects and emit signals. The
 default is None, which enables tracking but issues a warning that it will be disabled by default
 in the future. This requires extra memory and should be disabled if not needed.
-'''
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+"""
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Linking db instance from models.py
 db.init_app(app)
 
 
-# creating a db file before user accesses the server
-@app.before_first_request
-def create_table():
+with app.app_context():
     db.create_all()
 
 
-@app.route('/data/create', methods = ['GET', 'POST'])
-def create():
-    if request.method == 'GET':
-        return render_template('createpage.html')
+@app.route("/", methods=["GET"])
+def HomePage():
+    return render_template("home.html")
 
-    if request.method == 'POST':
-        employee_id = request.form['employee_id']
-        name = request.form['name']
-        age = request.form['age']
-        position = request.form['position']
-        employee = EmployeeModel(employee_id = employee_id, name=name, age=age, position=position)
+
+@app.route("/data/create", methods=["GET", "POST"])
+def create():
+    if request.method == "GET":
+        return render_template("createpage.html")
+
+    if request.method == "POST":
+        employee_id = request.form["employee_id"]
+        name = request.form["name"]
+        age = request.form["age"]
+        position = request.form["position"]
+        employee = EmployeeModel(
+            employee_id=employee_id, name=name, age=age, position=position
+        )
         db.session.add(employee)
         db.session.commit()
-        return redirect('/data')
+        return redirect("/data")
 
-@app.route('/data')
+
+@app.route("/data")
 def RetrieveDataList():
     employees = EmployeeModel.query.all()
-    return render_template('datalist.html', employees = employees)
+    return render_template("datalist.html", employees=employees)
 
-@app.route('/data/<int:id>')
+
+@app.route("/data/<int:id>")
 def RetrieveSingleEmployee(id):
     employee = EmployeeModel.query.filter_by(employee_id=id).first()
 
     if employee:
-        return render_template('data.html', employee = employee)
+        return render_template("data.html", employee=employee)
 
     return f"Employee with id ={id} Doenst exist"
 
-@app.route('/data/<int:id>/update', methods = ['GET', 'POST'])
+
+@app.route("/data/<int:id>/update", methods=["GET", "POST"])
 def update(id):
-    employee = EmployeeModel.query.filter_by(employee_id = id).first()
-    if request.method == 'POST':
+    employee = EmployeeModel.query.filter_by(employee_id=id).first()
+    if request.method == "POST":
         if employee:
             db.session.delete(employee)
             db.session.commit()
 
-            name = request.form['name']
-            age = request.form['age']
-            position = request.form['position']
-            employee = EmployeeModel(employee_id = id, name=name, age=age, position=position)
+            name = request.form["name"]
+            age = request.form["age"]
+            position = request.form["position"]
+            employee = EmployeeModel(
+                employee_id=id, name=name, age=age, position=position
+            )
 
             db.session.add(employee)
             db.session.commit()
 
-            return redirect(f'/data/{id}')
+            return redirect(f"/data/{id}")
 
         return f"Employee with id = {id} Does nit exist"
 
+    return render_template("update.html", employee=employee)
 
-    return render_template('update.html', employee = employee)
 
-
-@app.route('/data/<int:id>/delete', methods = ['GET', 'POST'])
+@app.route("/data/<int:id>/delete", methods=["GET", "POST"])
 def delete(id):
     employee = EmployeeModel.query.filter_by(employee_id=id).first()
-    if request.method == 'POST':
+    if request.method == "POST":
         if employee:
             db.session.delete(employee)
             db.session.commit()
-            return redirect('/data')
+            return redirect("/data")
         abort(404)
-    return render_template('delete.html')
+    return render_template("delete.html")
+
+
+if __name__ == "__main__":
+    app.run()
--- models.py
+++ models.py
@@ -2,12 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class EmployeeModel(db.Model):
-    __tablename__ = 'table'
+    __tablename__ = "table"
 
     id = db.Column(db.Integer, primary_key=True)
     employee_id = db.Column(db.Integer(), unique=True)
-    name= db.Column(db.String())
+    name = db.Column(db.String())
+    age = db.Column(db.Integer())
     position = db.Column(db.String(80))
 
     def __init__(self, employee_id, name, age, position):
